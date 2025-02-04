df773336459c23fd7a47394e4e497c53788294c9
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2025-02-04 10:23:05 +0100

    Add project files.

---
+++ wpf kajak kenu/Kajak.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_kajak_kenu
+{
+    class Kajak
+    {
+        public string Nev { get; set; }
+        public int Azonosito { get; set; }
+        public string Tipus { get; set; }
+        public int SzemelyekSzama { get; set; }
+        public int ElvitelOraja { get; set; }
+        public int ElvitelPerce { get; set; }
+        public int VisszahozatalOraja { get; set; }
+        public int VisszahozatalPerc { get; set; }
+
+        public Kajak(string sor)
+        {
+            var s = sor.Split(";");
+            Nev = s[0];
+            Azonosito = int.Parse(s[1]);
+            Tipus = s[2];
+            SzemelyekSzama = int.Parse(s[3]);
+            ElvitelOraja = int.Parse(s[4]);
+            ElvitelPerce = int.Parse(s[5]);
+            VisszahozatalOraja = int.Parse(s[6]);
+            VisszahozatalPerc = int.Parse(s[7]);
+        }
+        //nem jo
+        public bool Viz(int aktOra, int aktPerc)
+        {
+            int aktPercekben = aktOra * 60 + aktPerc;
+            int elvitelPercekben = ElvitelOraja * 60 + ElvitelPerce;
+            int visszahozatalPercekben = VisszahozatalOraja * 60 + VisszahozatalPerc;
+
+          
+            return aktPercekben >= elvitelPercekben + 1 && aktPercekben <= visszahozatalPercekben;
+        }
+
+
+    }
+}
---
+++ wpf kajak kenu/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_kajak_kenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Kajak> kajakok = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\kolcsonzes.txt",
+                encoding: Encoding.UTF8);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                kajakok.Add(new Kajak(sr.ReadLine()));
+            }
+            sr.Close();
+
+
+            KajakDataGrid.ItemsSource = kajakok;
+            
+         
+        }
+        private void EllenorzesGomb_Click(object sender, RoutedEventArgs e)
+        {
+            // Feltételezve, hogy van két TextBox az órához és perchez (OraTextBox, PercTextBox)
+            if (int.TryParse(OraTextBox.Text, out int ora) && int.TryParse(PercTextBox.Text, out int perc))
+            {
+                List<string> vizenLevok = new List<string>();
+
+                foreach (var kajak in kajakok)
+                {
+                    if (kajak.Viz(ora, perc))
+                    {
+                        vizenLevok.Add(kajak.Nev);
+                    }
+                }
+
+                if (vizenLevok.Count > 0)
+                {
+                    MessageBox.Show("Vizen lévő hajók:\n" + string.Join("\n", vizenLevok));
+                }
+                else
+                {
+                    MessageBox.Show("Ebben az időpontban egy hajó sincs vízen.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hibás időpont formátum! Kérlek, adj meg egy érvényes órát és percet.");
+            }
+        }
+
+
+
+
+    }
+}
